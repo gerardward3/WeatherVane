5c8cf1ed1daa0a5f1dd61c4fedf57555c895b98f
Gerard Ward <gerardward3@gmail.com>
2020-09-25 12:06:42 +0100

    adds weather data and description text

--- app/src/main/java/com/example/weathervane/MainActivity.kt
+++ app/src/main/java/com/example/weathervane/MainActivity.kt
@@ -1,20 +1,51 @@
 package com.example.weathervane
 
 import android.os.Bundle
-import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.TextView
+import com.example.weathervane.R.id.toolbar
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+import org.json.JSONArray
+import org.json.JSONObject
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        setSupportActionBar(findViewById(R.id.toolbar))
+        setSupportActionBar(findViewById(toolbar))
+        val windData = findViewById<TextView>(R.id.windData)
+        val description = findViewById<TextView>(R.id.description)
 
+        val queue = Volley.newRequestQueue(this)
+        val locationId: String = "2648579"
+        val url = "https://weather-broker-cdn.api.bbci.co.uk/en/mobile/$locationId"
 
+        val stringRequest = JsonObjectRequest(
+            Request.Method.GET, url, null,
+            Response.Listener { response ->
+                // Display the first 500 characters of the response string.
+                val forecasts = response.getJSONArray("forecasts")[0] as JSONObject
+                val summary = forecasts.getJSONObject("summary")
+                val report = summary.getJSONObject("report")
+                val windSpeedMph = report.getInt("windSpeedMph")
+                val windDescription = report.getString("windDescription")
+
+                val location = response.getJSONObject("location").getString("name")
+
+                windData.text = "Wind speed is $windSpeedMph miles per hour in $location."
+                description.text = "$windDescription."
+
+            },
+            Response.ErrorListener { windData.text = "That didn't work!" })
+
+        queue.add(stringRequest)
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
